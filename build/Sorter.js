7895764618460c9396265a7f770a2a8ed81fb6ff
Mitra <davoudabadi.mitra@gmail.com>
2022-10-29 12:16:35 -0600

    the abstract class added!

--- build/Sorter.js
+++ build/Sorter.js
@@ -2,15 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Sorter = void 0;
 class Sorter {
-    constructor(colloction) {
-        this.colloction = colloction;
-    }
     sort() {
-        const { length } = this.colloction;
+        const { length } = this;
         for (let i = 0; i < length; i++) {
             for (let j = 0; j < length - i - 1; j++) {
-                if (this.colloction.compare(j, j + 1)) {
-                    this.colloction.swap(j, j + 1);
+                if (this.compare(j, j + 1)) {
+                    this.swap(j, j + 1);
                 }
             }
         }
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
-import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
-// const numbersCollection = new NumbersCollection([0, 2, -20, 76]);
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
+import { LinkedList } from "./LinkedList";
 
-// console.log(numbersCollection.data);
+const numbersCollection = new NumbersCollection([0, 2, -20, 76]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection("sdjfbCD");
-const sorter = new Sorter(charactersCollection);
-sorter.sort();
+
+charactersCollection.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-18);
+linkedList.add(-3);
+linkedList.add(4);
+
+linkedList.sort();
+linkedList.print();
